ee429b6df95d1f0424bfebf2cf0d28c0d5e63b42
JTAtkins <jatkins@nerdplanet.com>
2018-01-24 21:48:30 -0600

    Stackify Library Setup

---
+++ Stackify/Stackify.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Stackify.h"
+
+
+Stackify::Stackify()
+{
+}
+
+
+Stackify::~Stackify()
+{
+}
---
+++ Stackify/Stackify.h
@@ -0,0 +1,8 @@
+#pragma once
+class Stackify
+{
+public:
+	Stackify();
+	~Stackify();
+};
+
